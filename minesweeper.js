bfab06dc7ee14c1182c90c789f918f7afe4f8870
anton-sales <anton.arus@gmail.com>
2018-04-30 17:05:49 +1200

    got board to display and could now play a part of the game with hidde: true.

--- minesweeper.js
+++ minesweeper.js
@@ -1,18 +1,36 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-// var board = 
+var board = {
+  cells: [
+    { row: 0, col: 0, isMine: 0, hidden: true },
+    { row: 0, col: 1, isMine: 0, hidden: true },
+    { row: 0, col: 2, isMine: 0, hidden: true },
+    { row: 1, col: 0, isMine: 0, hidden: true },
+    { row: 1, col: 1, isMine: 0, hidden: true },
+    { row: 1, col: 2, isMine: 0, hidden: true },
+    { row: 2, col: 0, isMine: 0, hidden: true },
+    { row: 2, col: 1, isMine: 0, hidden: true },
+    { row: 2, col: 2, isMine: 0, hidden: true },
 
-function startGame () {
+  ]
+}
+
+
+
+
+
+function startGame() {
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
+
 
 // Define this function to look for a win condition:
 //
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
-function checkForWin () {
+function checkForWin() {
 
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
@@ -27,6 +45,7 @@ function checkForWin () {
 //
 // It will return cell objects in an array. You should loop through 
 // them, counting the number of times `cell.isMine` is true.
-function countSurroundingMines (cell) {
+function countSurroundingMines(cell) {
 }
+
 
